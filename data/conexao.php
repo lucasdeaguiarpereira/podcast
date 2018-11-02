40fe7050fb8f2f93ec0435cb9ee185e0e7b786f0
principalprojeto <de.lucas73@gmail.com>
2018-11-02 17:29:18 -0300

    senha banco

--- data/conexao.php
+++ data/conexao.php
@@ -1,5 +1,5 @@
 <?php
- $conexao = mysqli_connect('localhost','root','super');
+ $conexao = mysqli_connect('localhost','root','');
  $banco = mysqli_select_db($conexao,'podcast');
  mysqli_set_charset($conexao,'utf8');
  
